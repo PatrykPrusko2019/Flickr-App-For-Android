9928596f838ba326aa6b53521cf9d1edc6eef347
PatrykPrusko2019 <patrykprusko@gmail.com>
2021-03-31 13:55:59 +0200

    :using Strings (from Resources) in the PhotoDetailActivity class

--- app/src/main/java/com/patrykprusko/flickrapp/MainActivity.java
+++ app/src/main/java/com/patrykprusko/flickrapp/MainActivity.java
@@ -66,7 +66,7 @@ public class MainActivity extends BaseActivity implements GetFlickrJsonData.OnDa
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_search) {
             return true;
         }
 
--- app/src/main/java/com/patrykprusko/flickrapp/PhotoDetailActivity.java
+++ app/src/main/java/com/patrykprusko/flickrapp/PhotoDetailActivity.java
@@ -1,5 +1,6 @@
 package com.patrykprusko.flickrapp;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -22,16 +23,23 @@ public class PhotoDetailActivity extends BaseActivity {
 
         Photo recordPhoto = (Photo) getIntent().getSerializableExtra(PHOTO_TRANSFER); // get object Photo
 
-        TextView photoAuthor = (TextView) findViewById(R.id.photo_author);
-        TextView photoTitle = (TextView) findViewById(R.id.photo_title);
-        TextView photoTags = (TextView) findViewById(R.id.photo_tags);
-        ImageView photoImage = (ImageView) findViewById(R.id.photo_image);
-
         if(recordPhoto != null) {
-            photoAuthor.setText("Author: " + recordPhoto.getAuthor());
-            photoTitle.setText("Title: " + recordPhoto.getTitle());
-            photoTags.setText("Tag: " + recordPhoto.getTags());
 
+            TextView photoAuthor = (TextView) findViewById(R.id.photo_author);
+            photoAuthor.setText( recordPhoto.getAuthor() );
+
+            TextView photoTitle = (TextView) findViewById(R.id.photo_title);
+//            photoTitle.setText("Title: " + recordPhoto.getTitle());
+            Resources resources = getResources();
+            String textResourceTitle = resources.getString(R.string.photo_title_text, recordPhoto.getTitle()); // strings.xml ->  <string name="photo_title_text">Title: %s</string>
+            photoTitle.setText(textResourceTitle);
+
+            TextView photoTags = (TextView) findViewById(R.id.photo_tags);
+//            photoTags.setText("Tag: " + recordPhoto.getTags());
+            String textResourceTags = resources.getString(R.string.photo_tags_text, recordPhoto.getTags()); // strings.xml ->  <string name="photo_tags_text">Tags: %s</string>
+            photoTags.setText(textResourceTags);
+
+            ImageView photoImage = (ImageView) findViewById(R.id.photo_image);
             Picasso.with(this).load(recordPhoto.getLinkLargeImage()).placeholder(R.drawable.placeholder_image) // get large image
                     .error(R.drawable.placeholder_broken_image).into(photoImage);
         }
